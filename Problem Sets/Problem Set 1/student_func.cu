2e2439bd1f89e8832da42015b837c13a3900db56
Amir <aemadzadeh@nvidia.com>
2017-11-13 17:48:43 -0800

    Fixed makefile. Getting a cuda error.

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -50,6 +50,19 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+
+    int x = blockDim.x * blockIdx.x + threadIdx.x; 
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
+    int tid = y * gridDim.x * blockDim.x + x;
+    int num_pixels = numRows * numCols;
+
+    if(tid < num_pixels){
+        unsigned char R = rgbaImage[tid].x;
+        unsigned char G = rgbaImage[tid].y;
+        unsigned char B = rgbaImage[tid].z;
+        float gray = .299f * R + .587f * G + .114f * B;
+        greyImage[tid] = (unsigned char) gray;
+    }
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,7 +70,7 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
+  const dim3 blockSize(numCols, numRows, 1);  //TODO
   const dim3 gridSize( 1, 1, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
