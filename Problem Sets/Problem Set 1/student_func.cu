c307b17d7a61fc42de7c78f34ed92bfeade93d85
Amir <aemadzadeh@nvidia.com>
2017-11-20 17:32:42 -0800

    Using ceil for computing grid/block dimension.

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -73,7 +73,7 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
   int DIM = 32;
-  const dim3 blockSize(numCols/DIM+1, numRows/DIM+1, 1);  //TODO
+  const dim3 blockSize((numCols + DIM - 1)/DIM, (numRows + DIM - 1)/DIM, 1);  //TODO
   const dim3 gridSize(DIM, DIM, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
